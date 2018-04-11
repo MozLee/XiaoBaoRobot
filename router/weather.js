02ee0646cae7977580803aa3af93a97f492e5371
mozlee <lixin@mozlee.com>
2018-04-11 18:08:44 +0800

    增加管理员普通用户路由

--- router/weather.js
+++ router/weather.js
@@ -2,7 +2,11 @@ const schedule = require('node-schedule') //时间任务管理 node-schedule
 const request = require('request');
 const dateTime = require('date-time');
 let tips = '小宝提醒您:北京春如四季,适当增减衣服，小心感冒哦~'
-function timeSentInfo(contact, contact2,contact3,contact4) {
+function timeSentInfo(users) {
+    if(!users instanceof Array){
+        console.error('天气ERR,用户信息出错');
+        return;
+    }
     schedule.scheduleJob('0 0 7 * * *', () => {
         let data = null;
         let url = 'https://api.seniverse.com/v3/weather/daily.json?key=9wf1etjmyn8kasuw&location=beijing&language=zh-Hans&unit=c&start=0&days=5'
@@ -27,11 +31,14 @@ function timeSentInfo(contact, contact2,contact3,contact4) {
             }else{
                 emj='/::~';
             }
-          let weatherModel = `亲爱哒~/:rose早上好[爱心]\n北京今日天气${emj}${w.text_day}\n最高气温${w.high}℃,最低气温${w.low}℃\n${w.wind_direction}💨指数${w.wind_scale}\n[Smart]${tips}`            
-            contact2.say(`亲爱哒~/:rose早上好[爱心]\n北京今日天气${emj}${w.text_day}\n最高气温${w.high}℃,最低气温${w.low}℃\n${w.wind_direction}💨指数${w.wind_scale}\n[Smart]${tips}\n千万不要忘记吃药奥~多喝热水，病就好拉~`);
-            contact.say(`${weatherModel}`);
-            contact3.say(`${weatherModel}`);
-            contact4.say(`${weatherModel}`);
+          let weatherModel = `亲爱哒~/:rose早上好[爱心]\n北京今日天气${emj}${w.text_day}\n最高气温${w.high}℃,最低气温${w.low}℃\n${w.wind_direction}💨指数${w.wind_scale}\n[Smart]${tips}`
+          users.forEach(user => {
+            user.say(`${weatherModel}`)
+          });            
+            // contact2.say(`亲爱哒~/:rose早上好[爱心]\n北京今日天气${emj}${w.text_day}\n最高气温${w.high}℃,最低气温${w.low}℃\n${w.wind_direction}💨指数${w.wind_scale}\n[Smart]${tips}\n千万不要忘记吃药奥~多喝热水，病就好拉~`);
+            // contact.say(`${weatherModel}`);
+            // contact3.say(`${weatherModel}`);
+            // contact4.say(`${weatherModel}`);
         })
         // console.log(data);
         // let weather = data.results[0].daily[0];
