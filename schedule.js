3e27bffa270eef4cdaeb96d7f1052188ccf86b2a
mozlee <lixin@mozlee.com>
2018-04-11 16:45:50 +0800

    删除无用文件

--- schedule.js
+++
@@ -1,9 +0,0 @@
-const schedule = require('node-schedule');
-
-function timeSentInfo() {
-    schedule.scheduleJob('5 * * * * *',() => {
-        console.log('定时任务'+new Date());
-    })
-};
-
-timeSentInfo();
