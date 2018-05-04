09d8c1ce2c6c1744bfdb5b193e2ef5f70938da8f
mozlee <lixin@mozlee.com>
2018-05-04 16:57:34 +0800

    修改schema，增加当前城市字段

--- schema/wechatusers.js
+++ schema/wechatusers.js
@@ -7,6 +7,7 @@ const WechatUser = new mongoose.Schema({
     sex:Number,//性别 1男 0女
     province:String,//省
     city:String,//城市
+    nowcity:String,//当前所在城市
     signature:String,//个性签名
     address:String,//地址
     star:Boolean,//星标好友
