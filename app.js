27b0d9935c39334f4bc42f1506f06e55b3a5edbe
mozlee <lixin@mozlee.com>
2018-05-07 14:00:39 +0800

    Server酱推送&&心跳防掉线测试

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //引入核心Wechaty
-const { Wechaty, Contact } = require("wechaty");
+const { Wechaty, Contact ,Room} = require("wechaty");
 
 //引入qrcode-terminal用于生成二维码
 const qrcodeTerminal = require("qrcode-terminal");
@@ -11,10 +11,18 @@ const dbconnection = require("./router/dbconnect");
 //引入User模型
 const User = require("./model/user");
 
-let n = 0;
+//引入时间模块
+const dayjs = require("dayjs");
+const dateTime = require('date-time');
+
+//引入server酱 微信提示错误以及登录等相关消息
+const serverChan = require('./server/ServerChan')
+
 let weatherService = false //初始天气服务
+let timer = null;
 //wechaty初始化
 Wechaty.instance()
+
   //扫描二维码阶段
   .on("scan", (url, code) => {
     if (!/200|201/.test(String(code))) {
@@ -22,17 +30,23 @@ Wechaty.instance()
       qrcodeTerminal.generate(loginUrl, {
         small: true
       }); //生成Terminal二维码
+
+      //server酱推送二维码
+      serverChan.login(url);
     }
     console.log(`${url}\n[${code}]扫描此url二维码登录`);
   })
+
   //成功登录阶段
   .on("login", async user => {
     console.log("---------------------------------------");
     console.log(`用户[${user.name()}]成功登录`);
+
     //连接数据库
     console.log("---------------------------------------");
     console.log("数据库连接中")
     await dbconnection();
+
     //天气
     const weather = require('./router/weather/weatherTask')
     if(weatherService)return;
@@ -41,21 +55,36 @@ Wechaty.instance()
         Contact
     })
     weatherService = true;
+
     //获取所有用户
     // const getAllUsers = require('./router/getAllUsers')
     // let a = await getAllUsers()
     // console.log(a.length);
+
+    //心跳信息防止掉线
+    const FDX = await Room.find({
+      topic:'FDX',
+    })
+    if(timer)return
+    timer = setInterval(() => {
+      FDX.say('XiaoBaoHeartBeat\n'+dateTime());
+      console.log(`[${dateTime()}]`+'发送心跳信息');
+    },1000*60*30) //每隔30分钟发一次
   })
   .on('message',async mes=>{
       let sender = await mes.from();
       let text = await mes.content();
       if(mes.self()){
           return
-      };      
-      console.log(`[${sender.name()}]:[${text}]`);
+      }; 
+      mes.say('小宝还在建设中，请耐心等待，当前提供天气服务。')     
+      console.log(`[${dateTime()}][${sender.name()}]:[${text}]`);
   })
-  .on('heartbeat',(data) =>   {
-        n++;
-        console.log('心跳包，我已经跳动'+n+'次');
+  .on('heartbeat',(data) =>  {
+
+  })
+  .on('logout',(user) => {
+     console.log(`${user.name()}登出${dateTime()}`);
+     serverChan.logout ();
   })
   .start();
